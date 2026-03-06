6cee4917f62d0a571e2d90152b929702be0977c7
Radmir <iradmir1516@gmail.com>
2026-03-06 20:51:14 +0500

    Update setup-nginx.sh

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -15,13 +15,17 @@ mkdir -p $VHOST_DIR
 # Создаем конфиг для gRPC и /api
 # Мы используем envsubst, чтобы подставить имя домена в название файла
 cat <<EOF > "${VHOST_DIR}/${DOMAIN}_location"
+
+# gRPC
 location /api {
     if (\$content_type !~ "application/grpc") {
-            return 404; # Отклоняем не-gRPC трафик на этом пути
-        }
+        return 404;
+    }
+
     if (\$scheme != "https"){
-            return 403;
-        }
+        return 403;
+    }
+
     client_max_body_size 0;
     client_body_timeout 1071906480m;
 
@@ -30,6 +34,24 @@ location /api {
 
     grpc_pass grpc://host.docker.internal:50051;
 }
-EOF
 
-echo "✅ Конфиг для ${DOMAIN} успешно создан в ${VHOST_DIR}"
+# WebSocket
+location /ws {
+    if (\$scheme != "https"){
+        return 403;
+    }
+
+    proxy_pass http://host.docker.internal:50052;
+
+    proxy_http_version 1.1;
+
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection "upgrade";
+
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+
+    proxy_read_timeout 86400;
+}
+
+EOF
